9c1cfc47e967a5404f4376a68f063d2945723a71
Dan Dsktp <daniel.grabowski@yahoo.com>
2025-01-29 20:36:40 -0500

    Coded Basic Linked List Classes

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,62 @@
 // Actual class for the linked list
-public class LinkedList {
+// TODO: Code insertAtPosition, removeValue, reverse, and length methods
+public class LinkedList<T> {
+    private Node<T> head;
+
+    public LinkedList(){
+        this.head = null;
+    }
+
+    public void insertAtHead(T value){
+        Node<T> newNode = new Node<>(value);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void printList(){
+        Node<T> current = head;
+        while(current != null){
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public void insertAtEnd(T value){
+        if(head == null){
+            insertAtHead(value);
+        }
+        else{
+            Node<T> newNode = new Node<>(value);
+            Node<T> current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public T removeHead(){
+        if(head == null){
+            return null;
+        }
+        T data = head.data;
+        head = head.next;
+        return data;
+    }
     
+    public boolean search(T value){
+        Node<T> current = head;
+        while(current != null){
+            if(current.data == value){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public void insertAtPosition(int pos, T value){
+
+    }
 }
--- Node.java
+++ Node.java
@@ -1,4 +1,10 @@
 // Node Class
-public class Node {
-    
+public class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    public Node(T data){
+        this.data = data;
+        this.next = null;
+    }
 }
